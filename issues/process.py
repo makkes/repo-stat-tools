b47e82ae738813d44c4e71e27876cfbd5f53135b
Max Jonas Werner <max@e13.dev>
2023-02-13 18:10:58 +0100

    lead time: add flag to use a fallback for the start time value
    Some issues are never moved to the "In Progress" column in Zenhub, for
    all kinds of different reasons. Those would usually be skipped in the
    lead time statistics but in case a user wants to include them, the new
    flag lets them state this expectation. In this case the start time is
    pulled from the issue creation time value.
    
    Since this may lead to a significant different in lead times, a
    warning is printed.

--- issues/process.py
+++ issues/process.py
@@ -2,7 +2,11 @@ import json
 import os
 import sys
 from datetime import datetime
+from enum import Enum
 
+class StartTimeSource(Enum):
+    IN_PROGRESS = 0
+    CREATED_AT = 1
 
 def load_issues(filename):
     if not os.path.exists(filename):
@@ -13,18 +17,18 @@ def load_issues(filename):
 
 def calculate_lead_time(issue):
     start_time = get_start_time(issue)
-    if start_time is None:
-        return None
     close_time = get_close_time(issue)
     if close_time is None:
+        print(f'skipping {issue["github"]["number"]} ({issue["github"]["title"]}): missing close data')
         return None
-    return (close_time - start_time).total_seconds()
+    return ((close_time - start_time[0]).total_seconds(), start_time[1])
 
 
 def get_start_time(issue):
     for event in issue["zenhub"]:
         if event["type"] == "transferIssue" and event["to_pipeline"]["name"] == "In Progress":
-            return datetime.strptime(event["created_at"], "%Y-%m-%dT%H:%M:%S.%fZ")
+            return (datetime.strptime(event["created_at"], "%Y-%m-%dT%H:%M:%S.%fZ"), StartTimeSource.IN_PROGRESS)
+    return (datetime.strptime(issue["github"]["created_at"], "%Y-%m-%dT%H:%M:%SZ"), StartTimeSource.CREATED_AT)
 
 
 def get_close_time(issue):
